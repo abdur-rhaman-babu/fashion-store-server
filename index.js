70ee56474d0824ca7391b7ecb1aca774d5faabbc
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-01 18:34:02 +0600

    set up mongodb user and password

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port =process.env.PORT || 4500
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 4500;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send('Fashion Server is running.......')
-})
+// fashionstore
+// sgGyhtRruuA5iAjU
 
-app.listen(port, ()=>{
-    console.log(`Fashion Server is Running on port ${port}`)
-})
+// const uri =
+//   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri =
+  `mongodb+srv://fashionstore:sgGyhtRruuA5iAjU@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Fashion Server is running.......");
+});
+
+app.listen(port, () => {
+  console.log(`Fashion Server is Running on port ${port}`);
+});
