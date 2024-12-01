6c718ea2ae6d6baa2b229835395ba3f712732396
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-01 21:59:54 +0600

    get fashion from database for update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
@@ -7,12 +8,8 @@ const port = process.env.PORT || 4500;
 app.use(cors());
 app.use(express.json());
 
-// fashionstore
-// sgGyhtRruuA5iAjU
-
-// const uri =
-//   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const uri = `mongodb+srv://fashionstore:sgGyhtRruuA5iAjU@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -46,6 +43,14 @@ async function run() {
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
         const result = await fashionCollection.deleteOne(query)
+        res.send(result)
+    })
+
+    // Get data for Update
+    app.get('/fashions/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await fashionCollection.findOne(query)
         res.send(result)
     })
 
