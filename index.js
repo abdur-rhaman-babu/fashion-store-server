755e55d4d5bc12676df4d76bc0a0f41ee7c9b327
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-01 15:39:18 +0600

    server set up

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port =process.env.PORT || 4500
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('Fashion Server is running.......')
+})
+
+app.listen(port, ()=>{
+    console.log(`Fashion Server is Running on port ${port}`)
+})
